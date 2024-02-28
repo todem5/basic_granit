31e8320e2a75cd4d5d984af65ba6f92c9f504abf
Dmitry Tolmachev <dmitrytolmachev@MacBook-Pro-Dmitry.local>
2024-02-28 21:59:08 +0600

    append

--- views/site/index.php
+++ views/site/index.php
@@ -10,15 +10,20 @@ $this->title = 'ГранитПро';
     <div class="body-content">
 
         <div class="row">
+            <h2>Фотогалерея</h2>
+            <div class="bodygrid">
             <div class="slider">
-                <p> #слайдер </p>
+	            <div class="box box-1" style="--img: url(/img/img1.png);" data-text="Одинарный 1"></div>
+	            <div class="box box-2" style="--img: url(/img/img2.png);" data-text="Одинарный 2"></div>
+	            <div class="box box-3" style="--img: url(/img/img3.png);" data-text="Семейный 1"></div>
+	            <div class="box box-4" style="--img: url(/img/img4.png);" data-text="Семейный 4"></div>
+	            <div class="box box-5" style="--img: url(/img/img5.png);" data-text="Семейный 5"></div>
+            </div>
             </div>
         </div>
         <div class="row">
-            <div>
-                <p> #категории товаров </p>
-            </div>
-            <div class="col-lg-4 mb-3">
+            <h2>Категории товаров</h2>
+            <div class="col-lg-12 mb-10">
                 <h2>Heading</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
@@ -29,9 +34,9 @@ $this->title = 'ГранитПро';
                 <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
             </div>
             <div>
-                <p> #Преимущества </p>
+            <h2>Преимущества</h2>
             </div>
-            <div class="col-lg-4 mb-3">
+            <div class="col-lg-12 mb-10">
                 <h2>Heading</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
@@ -42,17 +47,19 @@ $this->title = 'ГранитПро';
                 <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
             </div>
             <div>
-                <p> #О нас </p>
+            <h2>О нас</h2>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+            <div class="col-lg-4 mb-3">
+                <img class="img-responsive" src="<? echo Url::to('@web/img/base1.png')?>"></img>
+            </div>
+            <div class="col-lg-8 mb-7">
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p>С 2002 года наша компания занимается оптовой поставкой гранитных памятников производства Китая и России. ООО
+                «Аркадо-Гранит Новосибирск» предлагает качественный камень по выгодной цене. Приобретая готовые формы, Вы экономите
+                время, сокращаете свои производственные расходы и риски, освобождаетесь от необходимости контроля производственного
+                процесса.</p>
 
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <p><a class="btn btn-info" href="<?= Url::toRoute(['site/contact']) >">О компании &raquo;</a></p>
             </div>
         </div>
 
