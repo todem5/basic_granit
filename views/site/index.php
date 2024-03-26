fd2847ea54d51d8e26e79d0af925c070ffc79bc9
Dmitry Tolmachev <dmitrytolmachev@MacBook-Pro-Dmitry.local>
2024-03-26 08:43:49 +0600

    slider

--- views/site/index.php
+++ views/site/index.php
@@ -16,49 +16,59 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="row">
             <h2>Виды гранита: </h2>
             <div class="slider">
-
-                <div class="image">
-                    <div class="arrow left-arrow">
-                        <div class="short"></div>
-                        <div class="med"></div>
-                        <div class="long"></div>
+                <div class="list">
+                    <div class="item">
+                        <img src="/web/img/slider_0.jpg" alt="">
                     </div>
-                    <div class="arrow right-arrow">
-                        <div class="short"></div>
-                        <div class="med"></div>
-                        <div class="long"></div>
+                    <div class="item">
+                        <img src="/web/img/slider_1.jpg" alt="">
+                    </div>
+                    <div class="item">
+                        <img src="/web/img/slider_2.jpg" alt="">
+                    </div>
+                    <div class="item">
+                        <img src="/web/img/slider_3.jpg" alt="">
                     </div>
                 </div>
+                <div class="buttons">
+                    <button id="prev"><i class="fa fa-arrow-left" aria-hidden="true"></i></button>
+                    <button id="next"><i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+                </div>
+                <ul class="dots">
+                    <li class="active"></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
             </div>
-        </div>
 
-        <div class="row">
-            <h2>Категории товаров</h2>
-            <div class="d-flex flex-row justify-content-center">
+            <div class="row">
+                <h2>Категории товаров</h2>
+                <div class="d-flex flex-row justify-content-center">
 
-                <div class="p-2 flex-grow-1">
-                    <p><a class="btn btn-outline-secondary" href="<?= Yii::$app->urlManager->createUrl(["site/catalog"]) ?>" class="big-button">Каталог товаров</a></p>
+                    <div class="p-2 flex-grow-1">
+                        <p><a class="btn btn-outline-secondary" href="<?= Yii::$app->urlManager->createUrl(["site/catalog"]) ?>" class="big-button">Каталог товаров</a></p>
+                    </div>
+
                 </div>
 
-            </div>
-
-            <div>
-                <h2>О компании</h2>
-            </div>
-            <div class="d-flex flex-row justify-content-center">
-                <div class="p-2 flex-shrink-1 w-446">
-                    <img src="<?= Yii::$app->request->baseUrl . '/img/base1.png' ?>" class="img-fluid" alt="Responsive image">
+                <div>
+                    <h2>О компании</h2>
                 </div>
-                <div class="p-2 flex-grow-1">
+                <div class="d-flex flex-row justify-content-center">
+                    <div class="p-2 flex-shrink-1 w-446">
+                        <img src="<?= Yii::$app->request->baseUrl . '/img/base1.png' ?>" class="img-fluid" alt="Responsive image">
+                    </div>
+                    <div class="p-2 flex-grow-1">
 
-                    <p>С 2002 года компания занимается оптовой поставкой гранитных памятников производства Китая и России.
-                        ООО «ГранитПро» предлагает качественный камень по выгодной цене. Приобретая готовые формы, Вы экономите время, </p>
-                    <p><a class="btn btn-outline-secondary" href="<?= Yii::$app->urlManager->createUrl(["site/about"]) ?>" class="big-button">Подробнее</a></p>
+                        <p>С 2002 года компания занимается оптовой поставкой гранитных памятников производства Китая и России.
+                            ООО «ГранитПро» предлагает качественный камень по выгодной цене. Приобретая готовые формы, Вы экономите время, </p>
+                        <p><a class="btn btn-outline-secondary" href="<?= Yii::$app->urlManager->createUrl(["site/about"]) ?>" class="big-button">Подробнее</a></p>
+                    </div>
+
                 </div>
-
             </div>
+
         </div>
 
     </div>
-
-</div>
